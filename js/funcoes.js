affd81b54af134463ea6062d6a317f495c907e60
Júlia Aparecida <julia.aparecida25@gmail.com>
2021-04-06 22:19:57 -0300

    Criação da página de JavaScript

---
+++ js/funcoes.js
@@ -0,0 +1,17 @@
+function AdicionarFilme(){
+    let campoFilme = document.querySelector("#filme");
+    let imagemFilme = campoFilme.value;
+
+    if(imagemFilme.endsWith(".jpg")){
+        ListarFilmesNaTela(imagemFilme);
+    }else{
+        alert("Isso não é uma imagem!");
+    }
+} 
+
+function ListarFilmesNaTela(imagem){
+    let listaFilmes = document.querySelector("#listaFilmes");
+    let elementoFilme = "<img src=" + imagem + ">";
+    console.log(elementoFilme);
+    listaFilmes.innerHTML += elementoFilme;
+}
